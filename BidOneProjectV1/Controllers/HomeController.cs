9ef6ebe097a0143442d091e457f5df548cca4033
SnehaKhivansara <sneha.khivansara@exotel.in>
2023-03-12 23:42:30 +1300

    Form Submission test

---
+++ BidOneProjectV1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using BidOneProjectV1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace BidOneProjectV1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View("ProcessForm");
+        }
+
+        [HttpPost]
+        public IActionResult ProcessForm(IFormCollection form)
+        {
+            
+            var firstName = form["firstName"][0];
+            var lastName = form["lastName"][0];
+
+            _logger.LogInformation("Log message in the ProcessForm() method");
+
+            var jsonObject = new { FirstName = firstName, LastName = lastName };
+            string jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject);
+  
+            //string filePath = Path.Combine(HttpRuntime.AppDomainAppPath, "submittedData.json");
+            System.IO.File.WriteAllText(@".\path.json", jsonString);
+            return View("ProcessForm");
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BidOneProjectV1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BidOneProjectV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
